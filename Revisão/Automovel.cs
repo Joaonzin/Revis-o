c713b1d6cc9bf956650abfc813dcfb9b85431220
João Gabriel <163910348+Joaonzin@users.noreply.github.com>
2024-07-11 16:32:41 -0300

    Adicionar arquivos de projeto.

---
+++ Revisão/Automovel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisão
+{
+    public class Automovel
+    {
+        public string Marca { get; set; }
+
+        public string Modelo { get; set;}
+
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+    }
+}
---
+++ Revisão/Program.cs
@@ -0,0 +1,25 @@
+﻿using Revisão;
+
+Funcionario fun = new Funcionario();
+dependente den = new dependente();
+Automovel aut = new Automovel();
+
+int MatriculaFuncionario = fun.MatriculaFuncionario();
+Console.WriteLine(MatriculaFuncionario);
+
+fun.PlacaVeiculo = "JKL2784";
+if (fun.VerificarVeiculo(aut))
+{
+    Console.WriteLine("A placa contém 7 caracteres");
+}
+else
+{
+    Console.WriteLine("A Placa informada n contém 7 caracteres");
+}
+
+if (fun.Contatar())
+{
+    Console.WriteLine("Contato Realizado");
+}
+
+
